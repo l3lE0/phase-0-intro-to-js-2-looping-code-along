30fc7dd444aff036facb307aa0ebe9843a4106a0
Leonardo <leonardof.anzolas@gmail.com>
2023-12-26 11:04:14 -0500

    made functions to pass test

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+const thankYouMessage = []
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        thankYouMessage.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return thankYouMessage
+}
+
+function countDown() {
+
+    let i = 10
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
